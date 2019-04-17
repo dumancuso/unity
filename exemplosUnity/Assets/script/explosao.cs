ee987d3e800a7e2642397b4369d1d800febae8bf
DuMancuso <ed_mancuso@hotmail.com>
2019-04-17 17:27:04 -0300

    exercicio unity dia 17/4

---
+++ exemplosUnity/Assets/script/explosao.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosao : MonoBehaviour
+{
+   
+    public GameObject expl;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(expl, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ exemplosUnity/Assets/script/fimexplosao.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fimexplosao : MonoBehaviour
+{
+    private Animator anim;
+    public float TempoAnimacao;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        TempoAnimacao = anim.GetCurrentAnimatorStateInfo(0).length;
+        Destroy(gameObject, TempoAnimacao);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
